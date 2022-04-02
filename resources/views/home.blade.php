85bb341c8601dac661e86a67775c9a4f26a6efd5
marco <marco.introini@gmail.com>
2022-04-02 18:27:50 +0200

    Cleanup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,6 @@
 @section('contenuto-pagina')
     <div class="flex-col">
 
-        <div class="text-3xl">Funzioni di esempio</div>
-
-        <div class="my-4"><a href="{{route('modale-alpine')}}">Modale usando AlpineJs</a></div>
-        <div class="my-4"><a href="{{route('livewire-utenti')}}">Gestione utenti con livewire</a></div>
-        <div class="my-4"><a href="{{route('recapcha')}}">ReCapcha</a></div>
 
     </div>
 @endsection
