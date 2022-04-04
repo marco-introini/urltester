cfc0a51f5b740c64db29f35d766cad8a70723dd0
marco <marco.introini@gmail.com>
2022-04-04 17:45:17 +0200

    Added certificates disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'certificates' => [
+            'driver' => 'local',
+            'root' => storage_path('app/certificates'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
